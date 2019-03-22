d63379391ebaa01b72cf97c3bcd1f9c999e3b765
yono.ap <yono.ap@icloud.com>
2019-03-23 08:35:00 +0900

    Initial Commit

---
+++ Pure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pure
+//
+//  Created by yoshiyuki ono on 2019/03/23.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
