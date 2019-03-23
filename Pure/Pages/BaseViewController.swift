c7b2c11ef91d7ed9f63b7ffd0c46533031ef3c65
yono.ap <yono.ap@icloud.com>
2019-03-23 14:45:06 +0900

    add navigation

---
+++ Pure/Pages/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  Pure
+//
+//  Created by yoshiyuki ono on 2019/03/23.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        appTrace()
+    }
+}
--- Pure/ViewController.swift
+++ Pure/ViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
+/// Container ViewController
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         appTrace()
+
+        let viewController = HomeViewController()
+        viewController.title = "Home"
+        let navigationController = UINavigationController(rootViewController: viewController)
+        addChild(navigationController)
+        view.addSubview(navigationController.view)
+        navigationController.didMove(toParent: self)
     }
 
 }
