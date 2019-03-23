90981e781db1f643982f6ceb28ab1eeb9104b5fe
yono.ap <yono.ap@icloud.com>
2019-03-23 12:08:16 +0900

    add AppLog

--- Pure/AppDelegate.swift
+++ Pure/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        appTrace()
         return true
     }
 
@@ -25,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks.
         // Games should use this method to pause the game.
+        appTrace()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -32,21 +34,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate:
         // when the user quits.
+        appTrace()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state;
         // here you can undo many of the changes made on entering the background.
+        appTrace()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive.
         // If the application was previously in the background, optionally refresh the user interface.
+        appTrace()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate.
         // See also applicationDidEnterBackground:.
+        appTrace()
     }
 
 }
