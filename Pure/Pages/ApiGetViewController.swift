736e5ab7fd6249c92a97f764dbf67953dee19bc4
yono.ap <yono.ap@icloud.com>
2019-03-24 09:09:34 +0900

    add ApiGetViewController

---
+++ Pure/Pages/ApiGetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ApiGetViewController.swift
+//  Pure
+//
+//  Created by yoshiyuki ono on 2019/03/24.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import UIKit
+
+class ApiGetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
