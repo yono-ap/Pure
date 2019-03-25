7b0a587dac054ab0b174dcfc73a887ded1bf46e5
yono.ap <yono.ap@icloud.com>
2019-03-25 23:18:46 +0900

    add api

--- Pure/Pages/ApiGetViewController.swift
+++ Pure/Pages/ApiGetViewController.swift
@@ -9,21 +9,53 @@
 import UIKit
 
 class ApiGetViewController: BaseViewController {
+    @IBOutlet weak var labelDescription: UILabel!
+    @IBOutlet weak var labelUrl: UILabel!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var textViewResponse: UITextView!
+
+    @IBAction func clickGet(_ sender: UIButton) {
+        appTrace()
+        if indicator.isAnimating {
+            return
+        }
+        guard let url = URL(string: labelUrl.text ?? "") else {
+            appLog("url is nil.")
+            return
+        }
+
+        textViewResponse.text = ""
+        indicator.startAnimating()
+
+        DispatchQueue.global().async {
+            let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+                if let response = response {
+                    print(response)
+                    if let data = data {
+                        guard let text = String(data: data, encoding: String.Encoding.utf8) else {
+                            DispatchQueue.main.async { self.textViewResponse.text = "encoding failed." }
+                            return
+                        }
+                        DispatchQueue.main.async { self.textViewResponse.text = text }
+                    } else {
+                        DispatchQueue.main.async { self.textViewResponse.text = "data is nil." }
+                    }
+                } else {
+                    DispatchQueue.main.async { self.textViewResponse.text = "Error : \(String(describing: error))" }
+                }
+
+                DispatchQueue.main.async { self.indicator.stopAnimating() }
+            })
+            task.resume()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        labelDescription.text = "This page uses GitHub API as an example of URLSession.".localized
+        labelUrl.text = "https://api.github.com/users/yono-ap/repos"
+        textViewResponse.text = ""
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-
 }
