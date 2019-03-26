9fa12e8e45cdee1245f40e656e9fbc6827bd2576
yono.ap <yono.ap@icloud.com>
2019-03-26 19:55:26 +0900

    add JsonViewController

---
+++ Pure/Pages/JsonViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JsonViewController.swift
+//  Pure
+//
+//  Created by yoshiyuki ono on 2019/03/26.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import UIKit
+
+class JsonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
